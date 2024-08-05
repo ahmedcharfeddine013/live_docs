bebe3e66cbcb424c147050ac5b5283e52f480dde
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-08-05 22:27:36 +0100

    collaborative editor room

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -3,11 +3,28 @@ import Header from "@/components/Header";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 import CollaborativeRoom from "../../../../components/CollaborativeRoom";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getDocument } from "@/lib/actions/room.actions";
+import { metadata } from "../../../layout";
 
-const Document = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect("/");
+
+  // TODO: user access to document
+
   return (
     <main className="flex items-center w-full flex-col">
-      <CollaborativeRoom />
+      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
     </main>
   );
 };
