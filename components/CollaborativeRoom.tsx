a7742d0f6cad0a5c7810375175a135f870a932a6
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-08-06 01:29:41 +0100

    sharemodal

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,14 +10,14 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
+import ShareModal from "./ShareModal";
 
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
-  users, 
-  currentUserType
+  users,
+  currentUserType,
 }: CollaborativeRoomProps) => {
-
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
@@ -113,6 +113,12 @@ const CollaborativeRoom = ({
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollaborators />
+              <ShareModal
+                roomId={roomId}
+                collaborators={users}
+                creatorId={roomMetadata.creatorId}
+                currentUserType={currentUserType}
+              />
               <SignedOut>
                 <SignInButton />
               </SignedOut>
